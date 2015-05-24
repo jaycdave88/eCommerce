cce229bd9905e6dff97d466f575870b85671d76b
Jay Dave <jaycdave@gmail.com>
2015-05-24 00:58:50 -0700

    pushing to github - first commit / working ecom site

---
+++ app/overrides/change_title.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => 'spree/shared/_head',
+                     :name => 'override',
+                     :replace_contents => 'title',
+                     :text => 'Ecom - JD')
